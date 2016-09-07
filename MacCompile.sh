5d39d709c47bd960257cfd8c144107414b287675
doell4nuggets <doell4nuggets@gmail.com>
2016-09-07 14:48:00 -0600

    Pushing up an initial window and a display function

---
+++ MacCompile.sh
@@ -0,0 +1,1 @@
+g++ -std=c++14 -o main main.cpp -framework OpenGL -framework GLUT
--- main.cpp
+++ main.cpp
@@ -1,12 +1,35 @@
 #include <iostream>
 #include <iomanip>
+#include <cmath>
+#include <GLUT/glut.h>
 
 using namespace std;
 
-int main()
-{
-	cout << "Hello World!" << endl;
+// global vars
+int ScreenWidth  = 500;
+int ScreenHeight = 500;
 
-	return 0;
+void display( void );
+
+int main(int argc, char *argv[])
+{
+    glutInit(&argc, argv);                // initialize GLUT
+    glutCreateWindow("Tank Wards");		  // open window and specify title
+    glutDisplayFunc(display);			  // display callback: how to redisplay window
+    glutReshapeWindow(ScreenWidth, ScreenHeight);
+
+    glutMainLoop();
+
+    return 0;
 }
 
+void display( void )
+{
+    glClear(GL_COLOR_BUFFER_BIT);
+    gluOrtho2D( 0, 100, 0, 100 );
+
+    // Put drawing window here
+    // I have set the 2D coords to 100 by 100 but we can change this and will want to
+
+    glFlush();
+}
