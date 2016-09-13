89e265e8683e11cfca072391c046f13a90ea376a
Ben Kaiser <Ben@Bens-MacBook-Air.local>
2016-09-12 20:56:30 -0600

    constants.cpp renamed common.cpp

---
+++ Common.cpp
@@ -0,0 +1,15 @@
+
+#ifndef COMMON_TWBKTD
+#define COMMON_TWBKTD
+
+struct coordinate
+{
+    double coordinates[2];
+};
+
+const int ESC_KEY = 27;
+const int SPACE_KEY = 32;
+const int X_COORD = 0;
+const int Y_COORD = 1;
+
+#endif
