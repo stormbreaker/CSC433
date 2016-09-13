25efc8695dbd7787a08782aa459132b37358b9fa
Ben Kaiser <Ben@Bens-MacBook-Air.local>
2016-09-12 20:01:55 -0600

    mac compile supresses warnings

--- MacCompile.sh
+++ MacCompile.sh
@@ -1,7 +1,7 @@
 # g++ -std=c++14 main.cpp Tank.h Tank.cpp -framework OpenGL -framework GLUT
 
-g++ -std=c++14 -c main.cpp
-g++ -std=c++14 -c Tank.cpp
+g++ -std=c++14 -c main.cpp -w
+g++ -std=c++14 -c Tank.cpp -w
 g++ -std=c++14 main.o Tank.o -o TankWars -framework OpenGL -framework GLUT
 
 rm main.o
