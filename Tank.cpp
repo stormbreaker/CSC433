9d518776b347a880964a965f1885b7101b79206e
Ben Kaiser <Ben@Bens-MacBook-Air.local>
2016-09-22 20:17:11 -0600

    commenting Tank.cpp and terrain.h

--- Tank.cpp
+++ Tank.cpp
@@ -1,5 +1,16 @@
 #include "Tank.h"
 
+/*
+    Name: Tank
+    Author: Taylor Doell
+    Description:  This function is the constructor for the Tank class.  This class
+    contains all of the functionality and data need to create a tank.  The constructor
+    takes a boolean flag which tells wether or not the tank we are creating is
+    the left tank or not.  If it is, the initial firing angle is set to 45 degrees.
+    If it is not, then the initial firing angle is set to 135 degrees (45) degrees
+    facing to the left of the screen.  The constructor also sets the intial
+    velocity for both tanks to 100 m/s.
+*/
 Tank::Tank(bool isLeft)
 {
     // Check which tank to set correct starting firing angles
@@ -16,18 +27,38 @@ Tank::Tank(bool isLeft)
     velocity = 100;
 }
 
+/*
+    Name: SetFireCoords
+    Author: Taylor Doell
+    Description:  This function is simply a setter function for the private member
+    variable _ProjectilePath which contains the data necessary to draw the shooting
+    arc from each tank.
+*/
 void Tank::SetFireCoords(vector<Coordinate> &projectilePath)
 {
     // Store projectile coordinates for shooting the bullet
     _ProjectilePath = projectilePath;
 }
 
+/*
+    Name: GetFireCoords
+    Author: Taylor Doell
+    Description:  This function is simply a getter function for the private member
+    variable _ProjectilePath which contains the data necessary to draw the shooting
+    arc from each tank.
+*/
 vector<Coordinate> Tank::GetFireCoords()
 {
     // Return projectile coordinates for shooting the bullet
     return _ProjectilePath;
 }
 
+/*
+    Name: SetTankCoords
+    Author: Taylor Doell
+    Description:  This function sets up the body of the tank to be drawn.  It also
+    calls the other functions to draw the other parts of the tank.
+*/
 void Tank::SetTankCoords(int xValue, int yValue, bool isLeftTank)
 {
     double scale = 0.35;
@@ -88,6 +119,13 @@ void Tank::SetTankCoords(int xValue, int yValue, bool isLeftTank)
     }
 }
 
+/*
+    Name: DrawWheels
+    Author; Taylor Doell
+    Description:  This function sets up the coordinates to draw the wheels for
+    each individual tank.  It puts all of the coordinates into the DrawCoords
+    vector which is part of the tank class.
+*/
 void Tank::DrawWheels(int xValue, int yValue)
 {
     // Scale variable for easier math coordinates
@@ -129,6 +167,13 @@ void Tank::DrawWheels(int xValue, int yValue)
     DrawCoords.push_back(coord);
 }
 
+/*
+    Name: DrawBarrel
+    Author: Taylor Doell
+    Description:  This function sets up all of the coordinates to draw the barrel
+    for each of the tank.  It also adds all of the points defining the barrel into
+    the DrawCoords point.
+*/
 void Tank::DrawBarrel(int xValue, int yValue)
 {
     // Scale variable for easier math coordinates
@@ -165,6 +210,13 @@ void Tank::DrawBarrel(int xValue, int yValue)
     DrawCoords.push_back(coord);
 }
 
+/*
+    Name: DrawDome
+    Author: Taylor Doell
+    Description:  This function sets up all of the points which describe a tank's
+    dome.  All of the points are pushed into the tank's DrawCoords vector to be
+    called later to draw it.  
+*/
 void Tank::DrawDome(int xValue, int yValue)
 {
     // Scale variable for easier math coordinates
