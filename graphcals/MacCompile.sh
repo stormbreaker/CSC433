ff4768fae4fff4ce6ef326a87117833752402594
Ben Kaiser <Ben@Bens-MacBook-Air.local>
2016-10-04 21:31:46 -0600

    typed out code from book and added maccompile script

---
+++ graphcals/MacCompile.sh
@@ -0,0 +1,13 @@
+# g++ -std=c++14 main.cpp Tank.h Tank.cpp -framework OpenGL -framework GLUT
+
+#g++ -std=c++14 -c TankWars.cpp -w
+#g++ -std=c++14 -c Tank.cpp -w
+#g++ -std=c++14 -c Terrain.cpp -w
+#g++ -std=c++14 TankWars.o Tank.o Terrain.o -o TankWars -framework OpenGL -framework GLUT
+
+g++ -std=c++14 -c main.cpp -w
+g++ -std=c++14 main.o -o main -framework OpenGL -framework GLUT
+
+rm *.o
+
+./main
