ef300eb9ea3cae27a659987b167b34cc4135854b
Ben Kaiser <Ben@Bens-MacBook-Air.local>
2016-10-08 15:28:03 -0600

    removed imaginary output

--- graphcals/fractalview.cpp
+++ graphcals/fractalview.cpp
@@ -143,10 +143,6 @@ void winReshapeFcn (GLint newWidth, GLint newHeight)
 int main(int argc, char** argv)
 {
 
-	complex<double> ours(1,1);
-
-	ours = complexSquare(ours);
-	cout << ours << endl;
 
 	glutInit(&argc, argv);
 	glutInitDisplayMode(GLUT_SINGLE | GLUT_RGB);
