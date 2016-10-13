3b2a4129db289492423d9c3e5ef8a13af2300ce3
Ben Kaiser <Ben@Bens-MacBook-Air.local>
2016-10-12 18:18:30 -0600

    supressing warnings on maccompile script

--- graphcals/MacCompile.sh
+++ graphcals/MacCompile.sh
@@ -1,10 +1,10 @@
 #g++ -std=c++14 -c fractalview.cpp -w 
 #g++ -std=c++14 fractalview.o -o fractalviewer  -framework OpenGL -framework GLUT
 
-g++ -c -o fractalview.o fractalview.cpp
-g++ -c -o mandelbrot.o mandelbrot.cpp
-g++ -c -o julia.o julia.cpp
-g++ -c -o common.o common.cpp
+g++ -c -o fractalview.o fractalview.cpp -w
+g++ -c -o mandelbrot.o mandelbrot.cpp -w
+g++ -c -o julia.o julia.cpp -w
+g++ -c -o common.o common.cpp -w
 
 g++ -std=c++14 -o fractalviewer fractalview.o mandelbrot.o julia.o common.o -framework OpenGL -framework GLUT
 
