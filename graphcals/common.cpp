047a11f3ce77870ac02f19c4fc8f6d95e14dee21
Ben Kaiser <Ben@Bens-MacBook-Air.local>
2016-10-21 16:56:01 -0600

    common.cpp is documented

--- graphcals/common.cpp
+++ graphcals/common.cpp
@@ -23,11 +23,24 @@ bool isMandelbrotSet = true;
 bool isParallel = true;
 bool isAnimating = false;
 
+/*
+Name: plotPoint
+Author: Code from the book modified by Benjamin Kaiser and Taylor Doell
+Description: This function takes a single point in the structure format and the
+different components then get passed to the glVertex2d command to be printed on
+the screen.
+*/
 void plotPoint(Complex z)
 {
 	glVertex2d(z.x, z.y);
 }
 
+/*
+Name: complexSquareSerial
+Author: Code from the book modified by Benjamin Kaiser and Taylor Doell
+Description: This function takes a single point in the complex structure format
+and then computes the complex number multiplication operations to square the number
+*/
 Complex complexSquareSerial(Complex z)
 {
 	Complex zSquare;
@@ -38,6 +51,18 @@ Complex complexSquareSerial(Complex z)
 	return zSquare;
 }
 
+/*
+Name: SetColorAndPlot
+Author: Benjamin Kaiser and Taylor Doell
+Description:  This function takes the divergence limit (maxIter), the interations
+that the point was computed on, the point itself, and then a vector which describes
+a specific color palette.  There is then a large if-else statement which
+compares the actual number of iterations computed on the point to a range of the
+max iterations and assigns the correct color accordingly.  The color value associated
+with this specific point is then set and the current color utilized by OpenGL is
+set with that color.  The point is then passed to the plotPoint function which handles
+printing it on the screen.
+*/
 void SetColorAndPlot(int maxIter, int interations, Complex zPoint, vector<Color> colorSet)
 {
     Color ptColor(0, 0, 0);
@@ -75,6 +100,20 @@ void SetColorAndPlot(int maxIter, int interations, Complex zPoint, vector<Color>
 	plotPoint(zPoint);
 }
 
+/*
+name: zoom
+Author: Benjamin Kaiser and Taylor Doell
+Description:  This function is the function which handles the setting of all of the
+values associated with zooming.  It takes a percentage which is the percentage of
+the overall width/height taken off one side of the corresponding values (e.g. 10%
+of the overall width is taken off of each side of the width.  This makes it a 20%
+cut in total).  The new min and maxes are computed using this percentages and
+are added to or subtracted from respectively to get the new values.
+The distance of our current mouse coordinates from the coordinates that make
+up the center of the screen are computed and when we zoom in or out, the coordinates
+that our mouse was pointing at when the button was fired, move to the
+center of the screen and everything else moves with it.
+*/
 void zoom(double percentage)
 {
 		double xOffset, yOffset;
@@ -107,6 +146,16 @@ void zoom(double percentage)
 		gluOrtho2D(xComplexMin, xComplexMax, yComplexMin, yComplexMax);
 }
 
+/*
+Name: pan
+Author: Benjamin Kaiser and Taylor Doell
+Description:  This function handles the panning functionality for the screen.
+The function takes two offset values in pixels which are telling it how far to move
+in the left/right and up/down directions.  The cartesian coordinates of the center
+of the screen plus these offset values are computer and the
+x and y offsets are then computed and added to the complex min and maxes to slide
+the screen in the specific direction.
+*/
 void pan(int xPixOffset, int yPixOffset)
 {
 
@@ -132,6 +181,20 @@ void pan(int xPixOffset, int yPixOffset)
 	gluOrtho2D(xComplexMin, xComplexMax, yComplexMin, yComplexMax);
 }
 
+/*
+Name: getViewCoordinates
+Author: Benjamin Kaiser and Taylor Doell
+Description:  This function takes two values which are pixel coordinates on the
+device and converts them into cartesian coordinates in  the world scene.  This
+is done by calculating the width and height of the display in cartesian values.
+Since we assume that the thing will be square we use the width to calculate the
+amount of cartesian units that are displayed per pixel.  We then determine
+where the x and y axis lie in the pixel range and uses these to compute
+the overall x and y offsets from the center of the screen. After that we multiple
+the pixel coordinates passed us  minus the computed offset minus the center of the
+screen times the scale value.  This gives us the cartesian value that we clicked.
+This is then returned frm the function
+*/
 Complex getViewCoordinates(int x, int y)
 {
     Complex xyCoords;
@@ -150,7 +213,6 @@ Complex getViewCoordinates(int x, int y)
 
     scale = width / winWidth;
 
-	// these need to be figured out differently.... TODO since this is our major problem atm
     yAxis = winWidth - (xComplexMax / scale);
     xAxis = winHeight + (yComplexMin / scale);
 
