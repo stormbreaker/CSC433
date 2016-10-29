433dcd561f743b5ecbc3151c2c5c2c4121c54235
Ben Kaiser <Ben@Bens-MacBook-Air.local>
2016-10-28 19:45:40 -0600

    planet class getters and setters

--- stellaris/planet.cpp
+++ stellaris/planet.cpp
@@ -1,27 +1,57 @@
 #include "planet.h"
-#include "glutdefine.h"
-#include "common.h"
-#include <string>
+
 
 using namespace std;
 
-class Planet
+
+Planet::Planet()
 {
-    public:
-        void setRadius(double radius);
-        void setName(string name);
-        void setTextureImage(string filepath);
-        void setPlanetColor(Color planetColor);
+    Color tempColor;
+    tempColor.red = 0;
+    tempColor.green = 0;
+    tempColor.blue = 0;
+    _Radius = 0;
+    _Name = "Planet X";
+    _TextureImagePath = "";
+    _PlanetColor = tempColor;
+}
 
-        double getRadius();
-        string getName();
-        string getTextureImage();
-        Color getPlanetColor();
+void Planet::setRadius(double radius)
+{
+    _Radius = radius;
+}
 
+void Planet::setName(string name)
+{
+    _Name = name;
+}
 
-    private:
-        double radius;
-        string name;
-        string textureImage;
-        Color planetColor;
-};
+void Planet::setTextureImagePath(string filepath)
+{
+    _TextureImagePath = filepath;
+}
+
+void Planet::setPlanetColor(Color planetColor)
+{
+    _PlanetColor = planetColor;
+}
+
+double Planet::getRadius()
+{
+    return _Radius;
+}
+
+string Planet::getName()
+{
+    return _Name;
+}
+
+string Planet::getTextureImagePath()
+{
+    return _TextureImagePath;
+}
+
+Color Planet::getPlanetColor()
+{
+    return _PlanetColor;
+}
---
+++ stellaris/planet.h
@@ -0,0 +1,28 @@
+#include "glutdefine.h"
+#include "common.h"
+#include <string>
+
+using namespace std;
+
+class Planet
+{
+    public:
+        void setRadius(double radius);
+        void setName(string name);
+        void setTextureImagePath(string filepath);
+        void setPlanetColor(Color planetColor);
+
+        Planet();
+
+        double getRadius();
+        string getName();
+        string getTextureImagePath();
+        Color getPlanetColor();
+
+
+    private:
+        double _Radius;
+        string _Name;
+        string _TextureImagePath;
+        Color _PlanetColor;
+};
