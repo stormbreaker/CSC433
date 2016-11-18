a75cf582ef4d3c6ff2ed7fd0a3ea8e62878092e7
Ben Kaiser <Ben@Bens-MacBook-Air.local>
2016-11-17 22:43:59 -0700

    drawing files commented

--- stellaris/drawing.cpp
+++ stellaris/drawing.cpp
@@ -1,5 +1,16 @@
 #include "drawing.h"
 
+/*
+    Name: DrawPlanets
+    Author: Benjamin Kaiser and Taylor Doell
+    Description:  This function is the main drawing function of the entire program.
+    It loops through a vector that contains all of the planets and then calls
+    the different functions.  It checks for special cases such as earth moon and
+    sun and calls the special functions for those if necessary.  This function also
+    handles animation for incrementing values such as HourOfDay and DayOfYear for
+    the different planets.  This function also calls the drawing methods for the
+    orbital path values and the labels.
+*/
 void DrawPlanets()
 {
     int direction = -1;
@@ -17,6 +28,7 @@ void DrawPlanets()
                 planet.incrememtOrbitValues();
             }
 
+            // draw labels for everything but moon
             if (planet.getName() != "Moon")
             {
                 DrawOrbitRing(planet);
@@ -24,7 +36,7 @@ void DrawPlanets()
                 direction *= -1;
                 glutBitmapCharacter(GLUT_BITMAP_TIMES_ROMAN_24, 'i');
             }
-
+            // earth and moon are special cases since the moon depends on the earth
             if (planet.getName() == "Earth")
             {
                 glPushMatrix();
@@ -35,6 +47,8 @@ void DrawPlanets()
                 DrawMoon(planet);
                 glPopMatrix();
             }
+
+            // Saturn is special cased because of the rings
             else if (planet.getName() == "Saturn")
             {
                 DrawPlanet(planet, true);
@@ -47,6 +61,14 @@ void DrawPlanets()
     }
 }
 
+/*
+    Name: DrawPlanet
+    Author: Benjamin Kaiser and Taylor Doell
+    Description:  This function is the generic draw planet function and it takes
+    an instance of the planet class as well as whether or not this planet has
+    rings.  The material properties for this planet are set and if rings are drawn,
+    the properties for that are set as well.
+*/
 void DrawPlanet(Planet planet, bool drawRings)
 {
     GLUquadric* diskObject = gluNewQuadric();
@@ -58,14 +80,14 @@ void DrawPlanet(Planet planet, bool drawRings)
     float planetEmission[] = {0, 0, 0, 0};
     glMaterialfv( GL_FRONT_AND_BACK, GL_EMISSION, planetEmission );
     SetColor(planetColor.red, planetColor.green, planetColor.blue);
-    // glColor3ub(255, 255, 255);
     glDisable(GL_COLOR_MATERIAL);
 
-
+    //orbit
     glRotatef(360.0 * planet.getDayOfYear() / planet.getYear(), 0.0, 1.0, 0.0);
 
     glTranslatef(planet.getDistance(), 0.0, 0.0);
 
+    // daily rotation
     glRotatef(360.0 * planet.getHourOfDay() / planet.getDay(), 0.0, 1.0, 0.0);
 
     DrawSphere(planet);
@@ -73,22 +95,21 @@ void DrawPlanet(Planet planet, bool drawRings)
     if (drawRings == true)
     {
         diskObject = gluNewQuadric();
-		if (isTextured == true)
-		{
-			glEnable(GL_TEXTURE_2D);
-    		glTexParameterf( GL_TEXTURE_2D, GL_TEXTURE_WRAP_S, GL_REPEAT);
-    		glTexParameterf( GL_TEXTURE_2D, GL_TEXTURE_WRAP_T, GL_REPEAT);
-    		glTexParameterf( GL_TEXTURE_2D, GL_TEXTURE_MAG_FILTER, GL_NEAREST );
-    		glTexParameterf( GL_TEXTURE_2D, GL_TEXTURE_MIN_FILTER, GL_NEAREST );
-		    glTexEnvi( GL_TEXTURE_ENV, GL_TEXTURE_ENV_MODE, GL_MODULATE );
-			glTexImage2D(GL_TEXTURE_2D, 0, GL_RGB, ringTexture.width, ringTexture.height, 0, GL_RGB, GL_UNSIGNED_BYTE, ringTexture.imageDataArray);
-			gluQuadricTexture(diskObject, GL_TRUE);
-		}
-		else
-		{
-			glDisable(GL_TEXTURE_2D);
-		}
-		//gluDisk(diskObject, planet.getRadius() + 2, planet.getRadius() + 10, 20, 20);
+        if (isTextured == true)
+        {
+            glEnable(GL_TEXTURE_2D);
+            glTexParameterf( GL_TEXTURE_2D, GL_TEXTURE_WRAP_S, GL_REPEAT);
+            glTexParameterf( GL_TEXTURE_2D, GL_TEXTURE_WRAP_T, GL_REPEAT);
+            glTexParameterf( GL_TEXTURE_2D, GL_TEXTURE_MAG_FILTER, GL_NEAREST );
+            glTexParameterf( GL_TEXTURE_2D, GL_TEXTURE_MIN_FILTER, GL_NEAREST );
+            glTexEnvi( GL_TEXTURE_ENV, GL_TEXTURE_ENV_MODE, GL_MODULATE );
+            glTexImage2D(GL_TEXTURE_2D, 0, GL_RGB, ringTexture.width, ringTexture.height, 0, GL_RGB, GL_UNSIGNED_BYTE, ringTexture.imageDataArray);
+            gluQuadricTexture(diskObject, GL_TRUE);
+        }
+        else
+        {
+            glDisable(GL_TEXTURE_2D);
+        }
 
         gluCylinder(diskObject, planet.getRadius() + 10, planet.getRadius() + 2, 0, 20, 20);
     }
@@ -96,6 +117,15 @@ void DrawPlanet(Planet planet, bool drawRings)
     glPopMatrix();
 }
 
+/*
+    Name: DrawSun
+    Author: Taylor Doell and Benjamin Kaiser
+    Description:  This function is the special case drawing function for the sun.
+    It takes a planet instance and sets all of the lighting properties for the
+    solar system.  It sets the sun to emit a yello color and it also does not
+    allow the sun to spin.  The reason the sun is not allowed to spin is
+    because if it were, it was spinning super fast and was ugly looking.
+*/
 void DrawSun(Planet planet)
 {
     Color sunColor = planet.getPlanetColor();
@@ -119,16 +149,25 @@ void DrawSun(Planet planet)
     DrawSphere(planet);
 }
 
+/*
+    Name: DrawEarth
+    Author: Benjamin Kaiser and Taylor Doell
+    Description:  This function is the special case function for drawing the earth
+    It also sets the material properties and then draws the sphere.  Earth
+    and moon ahd to be special cases in order to get the moon to rotate about the
+    earth.
+*/
 void DrawEarth(Planet planet)
 {
     Color earthColor = planet.getPlanetColor();
 
+    // orbit
     glRotatef( 360.0 * planet.getDayOfYear() / planet.getYear(), 0.0, 1.0, 0.0 );
     glTranslatef( planet.getDistance(), 0.0, 0.0 );
     glPushMatrix();
-    // Second, rotate the earth on its axis. Use HourOfDay to determine its yRotation.
+
+    // rotation on axis
     glRotatef( 360.0 * planet.getHourOfDay() / planet.getDay(), 0.0, 1.0, 0.0 );
-    // Third, draw the earth as a wireframe sphere.
 
     glEnable(GL_COLOR_MATERIAL);
     float planetEmission[] = {0, 0, 0, 0};
@@ -141,12 +180,25 @@ void DrawEarth(Planet planet)
     glPopMatrix();
 }
 
+/*
+    Name: DrawMoon
+    Author: Benjamin Kaiser and Taylor Doell
+    Description:  This function is the special case function for the moon.  This
+    had to be a special function and it has to be called immediately after
+    drawing the earth because it does not push on or pop another matrix that would
+    cause it to work as another planet.
+*/
 void DrawMoon(Planet planet)
 {
     Color moonColor = planet.getPlanetColor();
 
+    // orbit the earth
     glRotatef( 360.0 * 12.0 * planet.getDayOfYear() / planet.getYear(), 0.0, 1.0, 0.0 );
     glTranslatef( planet.getDistance(), 0.0, 0.0 );
+
+    // rotate on axis
+    glRotatef( 360.0 * planet.getHourOfDay() / planet.getDay(), 0.0, 1.0, 0.0 );
+
 
     glEnable(GL_COLOR_MATERIAL);
     float planetEmission[] = {0, 0, 0, 0};
@@ -157,10 +209,18 @@ void DrawMoon(Planet planet)
     DrawSphere(planet);
 }
 
+/*
+    Name: DrawSphere
+    Author: Benjamin Kaiser and Taylor Doell
+    Description:  This function is the generalized function for drawing all of
+    the planets.  It checks to see if the global flags for textured, and solid vs
+    wireframe spheres are set.  If they are, then they draw things appropriately
+*/
 void DrawSphere(Planet planet)
 {
     GLUquadric* ball = gluNewQuadric();
 
+    // flip into upright position so axis of planet faces upwards
     glRotatef(90, 1.0, 0.0, 0.0);
 
 	if (isTextured == true)
@@ -189,6 +249,13 @@ void DrawSphere(Planet planet)
     }
 }
 
+/*
+    Name: DrawOrbitRing
+    Author: Benjamin Kaiser and Taylor Doell
+    Description:  This function draws the orbital path rings of each of the planets.
+    Material properties had to be set here to make the text not be affected by the
+    lighting.
+*/
 void DrawOrbitRing(Planet planet)
 {
     GLUquadric* diskObject = gluNewQuadric();
@@ -206,6 +273,13 @@ void DrawOrbitRing(Planet planet)
     glRotatef(-90.0, 1.0, 0.0, 0.0);
 }
 
+/*
+    Name: SetColor
+    Author: Benjamin Kaiser and Taylor Doell
+    Description:  This function is a helper function for the drawing functions
+    and setting material properties.  It takes 3 ints that make up and RGB
+    and then sets the current GL draw color to those values.
+*/
 void SetColor(int red, int green, int blue)
 {
     glEnable(GL_COLOR_MATERIAL);
@@ -215,13 +289,20 @@ void SetColor(int red, int green, int blue)
     glDisable(GL_COLOR_MATERIAL);
 }
 
+/*
+    Name: DrawStrokeString
+    Author: Benjamin Kaiser and Taylor Doell
+    Description:  This function draws text for the planetary orbital labels.
+    It is pretty well inline commented but there are some material properties
+    that need to be set here as well so that the lighting model does not affect
+    them similar to the orbital ring patterns.  
+*/
 void DrawStrokeString(string textToPrint, float x, float y, int fontSize)
 {
     // Set font size scaling to change the size
     double fontScaling = fontSize / 100.0;
     SetColor(255, 255, 255);
 
-    //glColor3ub(255, 255, 255); // Set text color to white
     glEnable(GL_COLOR_MATERIAL);
     float ringEmission[] = {1, 1, 1, 0};
     glMaterialfv( GL_FRONT_AND_BACK, GL_EMISSION, ringEmission );
