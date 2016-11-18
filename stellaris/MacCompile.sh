c127d25a9c9a7220304ef5f631d6b3402f0cbbfb
Ben Kaiser <Ben@Bens-MacBook-Air.local>
2016-11-17 21:55:13 -0700

    drawing functions broken out

--- stellaris/MacCompile.sh
+++ stellaris/MacCompile.sh
@@ -3,8 +3,9 @@ g++ -c -o solar.o solar.cpp -w
 g++ -c -o common.o common.cpp -w
 g++ -c -o imagemanip.o imagemanip.cpp -w
 g++ -c -o events.o events.cpp -w
+g++ -c -o drawing.o drawing.cpp -w
 
-g++ -std=c++14 -o solar planet.o solar.o common.o imagemanip.o events.o -framework OpenGL -framework GLUT
+g++ -std=c++14 -o solar planet.o solar.o common.o imagemanip.o events.o drawing.o -framework OpenGL -framework GLUT
 
 rm *.o
 
